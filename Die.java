9577cf4c05d48bdf1eb33572b10e294fe52aa0e9
Alexjandropiedra <36967884+Alexjandropiedra@users.noreply.github.com>
2018-12-04 20:38:59 -0700

    Update Die.java
    updated die class

--- Die.java
+++ Die.java
@@ -3,14 +3,19 @@ package alex.andrew.monoplyproject;
 import java.util.Random;
 
 public class Die {
+		public boolean doubleRoll = false;
+
 	public int roll() {
 		Random random = new Random();
+		int rolledDouble = 0;
 		int die1 = random.nextInt(7 - 1) + 1;
 		int die2 = random.nextInt(7 - 1) + 1;
+		if (die1 == die2) {
+			doubleRoll = true;
+			rolledDouble++;
+		}
+		doubleRoll = false;
 
-//		if (die1 == die2) {
-//			roll();
-//		}
 		int totalMove = die1 + die2;
 		return totalMove;
 	}
